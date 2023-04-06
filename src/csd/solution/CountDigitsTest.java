b8f4e55d634bb5accc2945239fcafdd76bfa5674
Busel Aydın <71033829+buselaydin@users.noreply.github.com>
2023-04-07 00:06:23 +0300

    isPrime, getPrime and facrorial added

--- src/csd/solution/CountDigitsTest.java
+++ src/csd/solution/CountDigitsTest.java
@@ -5,18 +5,13 @@ class CountDigitsTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.println("Sayı giriniz: ");
+		int val = kb.nextInt();
 		
-		int val;
-		System.out.print("Enter a number: ");
+		System.out.print(NumberUtil.factorial(val));
 		
-		while((val = Integer.parseInt(kb.nextLine())) != 0) {
-			
-			System.out.printf("%d of digits : %d%n",val, NumberUtil.countDigits(val));
-			
-			System.out.printf("%d of digits sum : %d%n",val, NumberUtil.sumDigits(val));
-			
-			System.out.print("Enter a number : ");
-		}
+		System.out.println();
+		System.out.print("of course i do it again :)");
 	}
 }
 class NumberUtil {
@@ -26,12 +21,107 @@ class NumberUtil {
 		int count = 0;
 		val = Math.abs(val);
 		
-		while(val != 0) {
+		while (val != 0) {
 			val /= 10;
 			count++;
 				
 		}
 		return count;	
+	}
+	
+	public static boolean isArmstrong(int val)
+	{
+		return val >= 0 && getDigitsPowSum(val) == val;
+	}
+	
+	public static int getDigitsPowSum(int val)
+	{
+		int total = 0;
+		int count = countDigits(val);
+	
+		for (int i = 1; i <= count; i++) {
+			total += pow((val % 10), count);
+			val /= 10;
+		}
+		return total;
+	}
+	public static boolean isPrime(long val)
+	{
+		if (val <= 1)
+			return false;
+		
+		if (val % 2 == 0)
+			return val == 2;
+		
+		if (val % 3 == 0)
+			return val == 3;
+		
+		if (val % 5 == 0)
+			return val == 5;
+		
+		if (val % 7 == 0)
+			return val == 7;
+		
+		for (long i = 11; i * i < val; i += 2) {
+			if (val % i == 0)
+				return false;
+		}
+		return true;
+		
+	}
+	
+	public static long factorial(int val)
+	{
+		long result = 1;
+		
+		if (val > 0)
+			for (int i = 1; i <= val; i++)
+				result *= i;
+		
+		return result;
+	}
+	
+	public static long getPrime(int n)
+	{
+		long val = 2;
+		int count = 0;
+		
+		do {
+			if (isPrime(val))
+				count++;
+			val++;
+		}while(count != n);
+		
+		return val-1;
+	}
+	
+	public static boolean isPrimeSlow(long val)
+	{
+		if(val > 1) {
+			for (long i = 2; i <= val / 2; i++) 
+				if(val % i == 0)
+					return false;
+			return true;
+		}
+		return false;
+	}
+	
+	public static int pow(int a, int b)
+	{
+		if(b < 0)
+			return 1;
+		
+		int result = 1;
+		
+		/*for (int i = 0; i < b; i++) 
+			result *= a;
+		
+		return result;*/
+		
+		while (b-- > 0) 
+			result *= a;
+		
+		return result;
 	}
 	
 	public static int sumDigits(int val)
@@ -56,4 +146,5 @@ class NumberUtil {
 		}
 		return result;
 	}
+	
 }
