a055072c25c07d4da938abcc59de269447d86feb
Busel Aydın <71033829+buselaydin@users.noreply.github.com>
2023-06-16 19:43:54 +0300

    FindAverageApp added.

---
+++ src/csd/solution/FindAverageApp.java
@@ -0,0 +1,25 @@
+package csd.solution;
+
+public class FindAverageApp {
+	public static void run() 
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Enter a number:");
+		
+		int number;
+		int sum = 0;
+		int count = 0;
+		
+		while ((number = kb.nextInt()) != 0) {
+			sum += number; 
+			count++;
+		}
+		double average = (double)sum / count;
+		
+		System.out.printf("Average: %.2f", average);
+		
+	}
+
+
+}
