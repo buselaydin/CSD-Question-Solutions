b5c58e6333150bb0167ed6bc20b135b070a96259
Busel Aydın <71033829+buselaydin@users.noreply.github.com>
2023-06-16 19:41:30 +0300

    FindAverageApp added.

--- src/csd/homework/App.java
+++ src/csd/homework/App.java
@@ -3,6 +3,6 @@ package csd.homework;
 class App {
 	public static void main(String [] args)
 	{		
-		Homework2.run();
+		Homework3.run();
 	}
 }
--- src/csd/solution/App.java
+++ src/csd/solution/App.java
@@ -3,6 +3,6 @@ package csd.solution;
 class App {
 	public static void main(String [] args)
 	{		
-		CountDigitsTest.run();
+		FindAverageApp.run();
 	}
 }
